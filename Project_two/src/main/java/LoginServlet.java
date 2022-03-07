8b3dc13d10c3b0da61d2f6dbcb4073147fe2f570
Curtis Morris <curtis592@revature.net>
2022-03-07 15:41:08 -0500

    upload java

---
+++ Project_two/src/main/java/LoginServlet.java
@@ -0,0 +1,41 @@
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        request.getRequestDispatcher("navbar.html").include(request,response);
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if(username.equals("admin") && password.equals("123")) {
+            out.println("You are successfully logged in.");
+            out.println("<br>Welcome " + username);
+
+            //using session
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+
+        } else {
+            out.println("Sorry! invalid details.");
+            RequestDispatcher rd = request.getRequestDispatcher("/login.html");
+            rd.include(request, response);
+        }
+
+        out.close();
+
+    }
+
+}
